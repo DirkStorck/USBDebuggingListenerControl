8f8d220e3d5ddb30e746171fa08740e7bb917e33
Dirk Storck <Dirk.Storck@googlemail.com>
2013-07-01 10:51:27 +0200

    xposed_init, XposedBridgeApi.jar

---
+++ USBDebuggingListenerControl/gen/de/dst/xposed/usbdebugginglistenercontrol/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.dst.xposed.usbdebugginglistenercontrol;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
